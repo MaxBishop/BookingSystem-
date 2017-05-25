fe1a46313e0d2ab8261f9bb04b3733107585c893
MaxBishop <maxbishop3@gmail.com>
2017-05-25 17:55:35 +0100

    iteration2-work on mail sender

--- Template_NOF9/TechnicalServices/SMTPMailServer.cs
+++ Template_NOF9/TechnicalServices/SMTPMailServer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,12 +12,14 @@ namespace TechnicalServices
         public class SMTPMailServer :  IEmailSender
         {
             private static string SMTP_HOST_NAME = "Smtp.gmail.com";
-            private static string SMTP_USER = "bishop3@gmail.com";
+            private static string SMTP_USER = "maxbishop3@gmail.com";
 
             public void SendTextEmail(string toEmailAddress, string text)
             {
                 SmtpClient client = new SmtpClient();
                 client.Host = SMTP_HOST_NAME;
+                client.EnableSsl = true;
+                client.Credentials = new NetworkCredential("maxbishop3@gmail.com", "password"); //to do
 
                 MailMessage message = new MailMessage();
                 message.Sender = new MailAddress(SMTP_USER);
