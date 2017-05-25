c7054c9e27af9a79829933d76731b715e81e9636
MaxBishop <maxbishop3@gmail.com>
2017-05-25 17:59:02 +0100

    Delete all bin, obj & packages

--- Template_NOF9/TechnicalServices/SMTPMailServer.cs
+++ Template_NOF9/TechnicalServices/SMTPMailServer.cs
@@ -12,14 +12,14 @@ namespace TechnicalServices
         public class SMTPMailServer :  IEmailSender
         {
             private static string SMTP_HOST_NAME = "Smtp.gmail.com";
-            private static string SMTP_USER = "maxbishop3@gmail.com";
+            private static string SMTP_USER = "bishop@gmail.com";
 
             public void SendTextEmail(string toEmailAddress, string text)
             {
                 SmtpClient client = new SmtpClient();
                 client.Host = SMTP_HOST_NAME;
                 client.EnableSsl = true;
-                client.Credentials = new NetworkCredential("maxbishop3@gmail.com", "password"); //to do
+                client.Credentials = new NetworkCredential("bishop@gmail.com", "password"); //to do
 
                 MailMessage message = new MailMessage();
                 message.Sender = new MailAddress(SMTP_USER);
