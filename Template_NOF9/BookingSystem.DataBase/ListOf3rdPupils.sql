d65c805c73cf8e35b65caf3f3f9396e248ef6d6e
Richard Pawson <rpawson@nakedobjects.net>
2017-10-12 11:54:22 +0100

    This IS Max's work, but committed from my laptop
    Max's laptop was broken, so he worked this lesson on my laptop

---
+++ Template_NOF9/BookingSystem.DataBase/ListOf3rdPupils.sql
@@ -0,0 +1,7 @@
+﻿select FullName
+from Pupils
+Where Pupils.Form = 0
+-- This is a list of all pupils from a year group. 
+-- Staff can refur to this when giving items to every pupil of a certain year group e.g. stationary packs
+-- Staff were unable to do this before without contacting admissions dep. this will be quicker and esier
+-- form is zero because the database converts enums to an integer so the firs yeargroup is represented with a 0
---
+++ Template_NOF9/BookingSystem.DataBase/SQLQuery3.sql
@@ -0,0 +1,3 @@
+﻿select Orders.Id , ProductName
+From OrderLines , Orders, Products
+where OrderLines.Order_Id = Orders.Id and OrderLines.ProductID = Products.ProductID
